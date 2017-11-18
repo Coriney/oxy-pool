efb40c6c5f9e5412645ef7914e5b33750b8af534
Coriney <33368141+Coriney@users.noreply.github.com>
2017-11-18 13:48:04 +0900

    Update liskpool.py

--- liskpool.py
+++ liskpool.py
@@ -105,7 +105,7 @@ def estimatePayouts (log):
     			payouts.append ({ "address": x['address'], "balance": (float (x['balance']) / 100000000 * forged) * 90 / 92 / weight})
 		elif x['address'] in conf['91']:
     			payouts.append ({ "address": x['address'], "balance": (float (x['balance']) / 100000000 * forged) * 91 / 92 / weight})
-		elif :
+		else :
     			payouts.append ({ "address": x['address'], "balance": (float (x['balance']) / 100000000 * forged) / weight})
 		payouts.append ({ "address": x['address'], "balance": (float (x['balance']) / 100000000 * forged) / weight})
 		#print (float (x['balance']) / 100000000, payouts [x['address']], x['address'])
